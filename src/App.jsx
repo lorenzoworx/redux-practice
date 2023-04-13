5a7cce641a6f60534c695003bf9879014c1f238f
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-04-13 18:43:10 +0100

    build app cart

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Navbar from './components/NavBar';
+import CartContainer from './components/CartContainer';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+
+function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
